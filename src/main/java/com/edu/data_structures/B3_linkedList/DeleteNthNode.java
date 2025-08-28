17fd1fadbbe908b681ec6e8a8e17428c5de63ec3
Akanov222 <akanov.gazproekt@gmail.com>
2025-08-28 20:08:31 +0300

    Update

---
+++ src/main/java/com/edu/data_structures/B3_linkedList/DeleteNthNode.java
@@ -0,0 +1,73 @@
+package com.edu.data_structures.B3_linkedList;
+
+public class DeleteNthNode {
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(3);
+        ListNode node3 = new ListNode(5);
+        ListNode node4 = new ListNode(7);
+
+        node1.setNext(node2);
+        node2.setNext(node3);
+        node3.setNext(node4);
+
+        printNode(node1);
+        node1 = deleteNthNode(node1, 2);
+        printNode(node1);
+    }
+
+    public static ListNode deleteNthNode(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.setNext(head);
+
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+        for (int i = 0; i < n; i++) {
+            fast.setNext(fast.getNext().getNext());
+        }
+
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return dummy.next;
+
+    }
+
+    public static void printNode(ListNode head) {
+        ListNode cursor = head;
+        while (cursor != null) {
+            System.out.print(cursor.getVal() + " ->");
+            cursor.setNext(cursor.getNext().getNext());
+        }
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public int getVal() {
+            return val;
+        }
+
+        public void setVal(int val) {
+            this.val = val;
+        }
+
+        public ListNode getNext() {
+            return next;
+        }
+
+        public void setNext(ListNode next) {
+            this.next = next;
+        }
+    }
+}
